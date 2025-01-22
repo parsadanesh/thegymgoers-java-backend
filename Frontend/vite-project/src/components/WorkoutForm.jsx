4b92fb33ee78f0a49bf790e2d73fb5a931db635a
pad31 <pad31@aber.ac.uk>
2025-01-22 16:51:11 +0000

    update

--- Frontend/vite-project/src/components/WorkoutForm.jsx
+++ Frontend/vite-project/src/components/WorkoutForm.jsx
@@ -19,7 +19,7 @@ const WorkoutForm = (props) => {
     const logWorkout = async (e) => {
         try {
 
-            const res = await axios.post(`https://gymgoers-spring.onrender.com/${props.user.username}/workouts`, 
+            const res = await axios.post(`https://gymgoers-spring.onrender.com/users/${props.user.username}/workouts`, 
                 {
                     exercises: exercises,
                     dateCreated: ""
