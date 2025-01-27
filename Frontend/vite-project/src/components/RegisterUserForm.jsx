f1a8af856cd879933d289c97cda51a18cd3d5223
pad31 <pad31@aber.ac.uk>
2025-01-27 11:44:23 +0000

    UPDATE: Fixed sign up confirmation message

--- Frontend/vite-project/src/components/RegisterUserForm.jsx
+++ Frontend/vite-project/src/components/RegisterUserForm.jsx
@@ -16,7 +16,7 @@ const RegisterUserForm = (props) => {
     return (
         <form onSubmit={handleSubmit}>
             <div style={{ marginBottom: '20px' }}>
-                <label htmlFor="email" style={{ marginRight: '10px' }}>Email: </label>
+                <label htmlFor="username" style={{ marginRight: '10px' }}>Username: </label>
                 <input
                 data-testid="email"
                 type="username"
