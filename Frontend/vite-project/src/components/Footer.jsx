1cc86d7625865acbddbf72b8dbde044ee5bb6867
pad31 <pad31@aber.ac.uk>
2025-01-27 13:20:19 +0000

    UPDATE: trying the fix the footer issue for smaller screens

--- Frontend/vite-project/src/components/Footer.jsx
+++ Frontend/vite-project/src/components/Footer.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import '../css/Footer.css'; 
+
 const Footer = () => {
 
+    //          <p className="col-md-4 d-flex align-items-center justify-content-center">The GymGoers</p>
     return (
         <>
-            <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top bg-dark text-success" >
-                <div className="col-md-4 d-flex align-items-center justify-content-center">The GymGoers</div>
+            <footer>
+                <p>The GymGoers</p>
 
             </footer>
         </>    
